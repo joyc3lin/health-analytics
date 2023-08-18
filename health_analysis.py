a2bade16913cd0240dd09b3cafcb81a1542074b1
joycelin0w0 <joyce.lin.1@stonybrook.edu>
2023-08-18 18:24:10 +0000

    added a for loop to run multiple citizens

--- health_analysis.py
+++ health_analysis.py
@@ -19,39 +19,35 @@ aboutBob = {
 
 def seniorCitizen(age, disability):
     if age >= 65 and disability == "yes":
-        eligiblity = "you are eligible for Medicare."
+        eligiblity = "you are eligible to recieve Medicare."
     elif age >= 65 and disability == "no":
-        eligibility = "you are eligible for Medicare."
+        eligibility = "you are eligible to recieve Medicare."
     elif age <65 and disability == "yes": 
-        eligibility = "you may be eligible for Medicare."
+        eligibility = "you may be eligible to recieve Medicare."
     else: 
-        eligibility = "you are not eligibe for Medicare."
+        eligibility = "you are not eligibe to recieve Medicare."
     return eligibility 
 
-Citizens = [
-    [64,"yes"],
-    [70, "no"],
-    [40, "no"],
-    [22, "yes"],
-    [65, "no"]
+citizens = [
+[64,"yes"],
+[70, "no"],
+[40, "no"],
+[22, "yes"],
+[65, "yes"]
 ]
 
+for citizen in citizens:
+    print('\n')
+    print(f'Now reviewing your data: age: {citizen[0]}, disability: {citizen[1]}')
+    
+    """age = citizen[0]
+    disability = citizen[1]
+    outputFunction = seniorCitizen(age,disability)
+    print(f'According to Medicare: {outputFunction}')
+    print('\n')"""
 
 
 
-
-    """ print("Your age:", age)
-        print("According to Medicare guidelines, you are eligible to receive Medicare.")
-    elif age >= 65 and disability == "no":
-        print("Your age:", age)
-        print("According to Medicare guidelines, you are eligible to receive Medicare.")
-    elif age <65 and disability == "yes": 
-        print("Your age:", age)
-        print("According to Medicare guidelines, you may be eligible to receive Medicare.")
-    else: 
-        print("Your age:", age)
-        print("According to Medicare guidelines, you are not eligible to receive Medicare.") 
-"""
 
 
 
