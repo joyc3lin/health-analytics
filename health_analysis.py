f80f1ecbaaf6fcf1dbf386a6ed4f7c274021f2d6
joycelin0w0 <joyce.lin.1@stonybrook.edu>
2023-08-18 17:00:11 +0000

    added list and dictionary

--- health_analysis.py
+++ health_analysis.py
@@ -3,3 +3,19 @@ import numpy as np
 
 num = 100
 word = 'pineapples are great'
+lis = ["this","is","a","list"]
+
+aboutBob = {
+    "bob" : "the main character",
+    "age" : 44,
+    "residence" : "apartment",
+    "jobs" : ["dog walker", "babysitter","president"],
+    "bobs_things": {
+        "phone": "this is bob's iphone 14",
+        "clothes": ["shirt","suit","shoes","a sock"],
+        "money" : 20.23
+
+    }
+}
+
+
